48b60d5d7d72fc11e5f56cd3b175ed95fe90fd99
Ksenia Krasheninnikova <ksenia@kolossus.sdsc.edu>
2014-10-18 23:13:06 -0700

    sample configs added; fixed io in gc track wrapper

--- bin/gc_track_wrapper.py
+++ bin/gc_track_wrapper.py
@@ -14,8 +14,9 @@ DATA_ROOT = parser.get('environment','DATA_ROOT')
 ORGANISM = parser.get('environment','ORGANISM')
 VERSION = parser.get('environment','VERSION')
 NAME=parser.get('environment','NAME')
-PATH_TO_FASTA_DIR = os.path.join(DATA_ROOT, 'assemblies', ORGANISM, VERSION)
-PATH_TO_OUTPUT = os.path.join(PATH_TO_FASTA_DIR, 'tracks', NAME, get_time())
+PATH_TO_FASTA_DIR = os.path.join(DATA_ROOT, 'assemblies', ORGANISM, VERSION, 'genome')
+PATH_TO_TRACK_DIR = os.path.join(DATA_ROOT, 'assemblies', ORGANISM, VERSION, 'tracks', NAME)
+PATH_TO_OUTPUT = os.path.join(PATH_TO_TRACK_DIR, get_time())
 
 def get_fasta_files_from_dir(path) :
 	files = os.listdir(path)
@@ -24,7 +25,7 @@ def get_fasta_files_from_dir(path) :
 
 
 def run_gc_track() :
-	if NAME != gc :
+	if NAME != 'gc' :
 		print 'config file is not for gc track'
 		print 'done.'
 	print 'starting wrapper for gc track from', sys.argv[0], '...'
