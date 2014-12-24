33415f09e150ae7ae8a9557012be4bb735c6bdfe
Ksenia Krasheninnikova <ksenia@kolossus.sdsc.edu>
2014-12-24 04:16:11 -0800

    calculate the anchors density

---
+++ bin/count_anchors_density.py
@@ -0,0 +1,50 @@
+#!/hive/groups/recon/local/bin/python
+
+#reads mgr_micro.txt file and calculates 
+#density of anchors comprising synteny blocks
+#i.e. length(sum of anchors lengths) / length(block_length)
+
+import sys
+import numpy
+
+#genome_id is one-based
+def parse_mgr_micro(path, genome_id) :
+    with open(path) as f:
+        anchor_lengths = []
+        block_lengths = []
+        skip_mgr = False
+        for line in f:
+            line = line.strip()
+            data = line.split()
+            if 'end_mgr' in line:
+                skip_mgr = False
+                continue
+            if skip_mgr :
+                continue
+            if 'begin_block' in line:
+                anchor_lengths.append(0.0)
+                base = 2 + (genome_id - 1) * 4
+                block_lengths.append(int(data[base + 3]))
+                continue
+            if 'begin_mgr' in line:
+                skip_mgr = True
+                continue
+            if '#' in line:
+                continue
+            base = (genome_id - 1) * 4
+            length = int(data[base + 3])
+            anchor_lengths[-1] += length
+    return anchor_lengths, block_lengths
+
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    genome_id = int(sys.argv[2])
+    anchor_lengths, block_lengths = parse_mgr_micro(path, genome_id)
+    rates = [a/b for a,b in zip(anchor_lengths, block_lengths)]
+    print 'min density', min(rates)
+    print 'max density', max(rates)
+    print 'median density', numpy.median(rates)
+    #for i in range(len(rates)):
+    #    print i+1, rates[i]
