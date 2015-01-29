494c4621f90da5ad7cb07e8ee2ffd5230b05813d
Ksenia Krasheninnikova <ksenia@kolossus.sdsc.edu>
2015-01-29 08:42:33 -0800

    scripts for finding and processing rearrangements

---
+++ bin/grimm_output_to_bed.py
@@ -0,0 +1,5 @@
+#!/hive/groups/recon/local/bin/python
+
+if __name__ == '__main__':
+       if len(sys.argv)< :
+           print 'USAGE:', sys.argv[0], 'grimm_output_file', 'blocks.txt', 'output.bed' 
