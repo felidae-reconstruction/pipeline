ba8b4dfc783576c463984cd000a8da547b25d8d5
Ksenia Krasheninnikova <ksenia@kolossus.sdsc.edu>
2015-02-02 03:25:13 -0800

    handling separate chromosomes for running liftover on cluster

--- bin/chain_scripts/run_halliftover.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-set -beEu -o pipefail; /hive/users/ksenia/apps/hal/bin/halLiftover --inMemory --outPSL $1 $2 $3 $4  /scratch/tmp/liftover.bed ;  pslPosTarget /scratch/tmp/liftover.bed $5; rm /scratch/tmp/liftover.bed
