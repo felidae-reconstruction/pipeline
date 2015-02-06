fdc632cb57dd839d529ed1f72224f47ac805f955
Ksenia Krasheninnikova <ksenia@kolossus.sdsc.edu>
2015-02-06 03:32:55 -0800

    cactus pipeline refactored

--- bin/cactus_runner.py
+++ bin/cactus_runner.py
@@ -8,12 +8,12 @@ import utils
 def run(config, dir, outputhal):
     utils.check_existence_or_raise(config)
     utils.create_dir_if_not_exists(dir)
-    command = 'runProgressiveCactus.sh --batchSystem parasol --bigBatchSystem singleMachine'\
-    ' --defaultMemory 8589934593 --bigMemoryThreshold 8589934592 --bigMaxMemory 893353197568'\
-    ' --bigMaxCpus 25 --maxThreads 25 --parasolCommand=\'/cluster/home/jcarmstr/bin/parasol -host=ku\''\
-    ' --retryCount 3'
-    print command
-    params = [command, config, dir, outputhal]
-    subprocess.call(" ".join(params), shell=True)
+    command = ['runProgressiveCactus.sh', '--batchSystem', 'parasol', '--bigBatchSystem', 'singleMachine',
+    '--defaultMemory', '8589934593', '--bigMemoryThreshold', '8589934592', '--bigMaxMemory', '893353197568',
+    '--bigMaxCpus', '25', '--maxThreads', '25', '--parasolCommand=/cluster/home/jcarmstr/bin/parasol -host=ku',
+    '--retryCount', '3']
+    params = command + [config, dir, outputhal]
+    print params
+    subprocess.check_call(params)
     return outputhal
 	
