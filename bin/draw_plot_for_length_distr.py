3a8ac6683e5898b10e50b216527e61586a02c343
Ksenia Krasheninnikova <ksenia@ku.sdsc.edu>
2015-06-24 01:54:25 -0700

    changed paramter parsing, methods to draw stats

--- bin/draw_plot_for_length_distr.py
+++ bin/draw_plot_for_length_distr.py
@@ -1,7 +1,11 @@
 #!/hive/groups/recon/local/bin/python
+'''
+script for calculation and drawing the length distribution for different synteny blocks values
+'''
 
 import bisect
 import sys
+import argparse
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib.backends.backend_pdf import PdfPages
@@ -12,12 +16,15 @@ import utils
 
 def draw_distr(x,y,name):
     pp=PdfPages(name)
-    plt.xlabel('length')
-    plt.ylabel('number of synteny blocks')
-    plt.tick_params(axis='both', which='major', labelsize=10)
+    plt.xlabel('Length')
+    plt.ylabel('Number of synteny blocks')
+    ##plt.tick_params(axis='both', which='major', labelsize=10)
+    plt.ticklabel_format(style='sci',scilimits=(0,0))
+    #major_formatter = plt.FormatStrFormatter('%2.1f')
+    #plt.gca().xaxis.set_major_formatter(major_formatter)
     plt.tick_params(axis='both', which='minor', labelsize=10)
-    #bp = plt.boxplot(lengths, labels=coverages)
-    bp = plt.plot(x,y)
+    ##bp = plt.plot(x,y)
+    plt.bar(x, y, align='center', width=100000, color='green')
     plt.savefig(pp, format='pdf')
     pp.close()
 
@@ -52,15 +59,14 @@ def extract_lengths(file):
     return values
 
 if __name__ == '__main__' :
-    if len(sys.argv) < 3:
-        print 'script for calculation and drawing the length distribution for different synteny blocks values'
-        print 'USAGE:', sys.argv[0], 'list_of_lengths', 'output_file_name'
-    file = sys.argv[1]
-    output = sys.argv[2]
-    values = extract_lengths(file)
-    print min(values), max(values), numpy.median(values)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file')
+    parser.add_argument('output')
+    args = parser.parse_args()
+    values = extract_lengths(args.file)
+    print 'min:', min(values), 'max:', max(values), 'median:', numpy.median(values)
     max_val = max(values)
-    x_range, values = process_lengths(values, 10000)
-    print len(values), len(x_range)
-    draw_distr(x_range,values,output)
+    x_range, values = process_lengths(values, 200000)
+    #print len(values), len(x_range)
+    draw_distr(x_range,values,args.output)
 
